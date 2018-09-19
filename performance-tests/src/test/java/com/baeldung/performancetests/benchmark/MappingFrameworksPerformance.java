f015b297ea6cb0446b1263efd445b8c6e9c5c214
mocaste <stefan_cosmin@hotmail.com>
2018-09-19 15:26:46 +0300

    revert change on test

--- performance-tests/src/test/java/com/baeldung/performancetests/benchmark/MappingFrameworksPerformance.java
+++ performance-tests/src/test/java/com/baeldung/performancetests/benchmark/MappingFrameworksPerformance.java
@@ -97,7 +97,7 @@ public class MappingFrameworksPerformance {
         sourceCode = new SourceCode("This is source code!");
     }
 
-    public static void main(String[] args) throws IOException, RunnerException {
+    public void main(String[] args) throws IOException, RunnerException {
         org.openjdk.jmh.Main.main(args);
 
     }
